4f84a19ddd5978602271fe4f4cd3de5346aea3f6
tgmike <tgmike77@gmail.com>
2021-05-19 22:40:29 -0700

    Added bottom card

---
+++ my-app/src/BottomWelcome.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './BottomWelcome.css';
+export default function BottomWelcome() {
+    return (
+        <div>
+            <section className="bottomCard star">
+                <h1>
+                    <strong> About Us </strong>
+                </h1>
+                
+                <p>
+                    We are a young group of software engineers passionate about helping people
+                </p>
+            </section>
+        </div>
+    )
+}
