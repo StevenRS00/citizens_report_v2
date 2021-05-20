5c13abf189ca971e688401b13899a2626c1d1d95
Alexis Sanchez <jwlexi>
2021-05-19 22:28:35 -0700

    Adding Top Card

---
+++ my-app/src/TopWelcome.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './TopWelcome.css';
+export default function TopWelcome() {
+    return (
+        <div className="topCard text-center">
+            <section className="star text-light WelcomeCard1">
+      <h1> Welcome to Citizens Report </h1>
+      <h3 className="p-2"> From us, for you. </h3>
+      <p className="text-muted"> Available now </p> 
+      
+      <form action="/login">
+        <button className="btn btn-primary">Log in</button>
+      </form>
+      
+      <span className="text-muted largeFont">or</span>
+      
+      <form action="/register">
+        <button className="btn btn-primary">Register</button>
+      </form>
+    </section>
+        </div>
+    )
+}
