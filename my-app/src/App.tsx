cfe6067cc2074d493fe8e72dc88661ea3ff44791
Steven Schreiber <sschreiber.ssteven@gmail.com>
2021-05-20 20:03:23 -0700

    Fixing App.tsx spacing

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -5,18 +5,21 @@ import TopWelcome from './TopWelcome'
 import {MiddleWelcome} from './MiddleWelcome'
 import BottomWelcome from './BottomWelcome'
 import FadeInAnimation from "./FadeIn";
+
 function App() {
   return (
     <div style={{backgroundColor: "black"}}>
       <FadeInAnimation wrapperElement="div" direction="down">
           <TopWelcome/>
-        </FadeInAnimation>
-        <FadeInAnimation wrapperElement="div" direction="up">
-          <MiddleWelcome />
-        </FadeInAnimation>
-        <FadeInAnimation wrapperElement="div" direction="up">
-          <BottomWelcome />
-        </FadeInAnimation>
+      </FadeInAnimation>
+
+      <FadeInAnimation wrapperElement="div" direction="up">
+        <MiddleWelcome />
+      </FadeInAnimation>
+      
+      <FadeInAnimation wrapperElement="div" direction="up">
+        <BottomWelcome />
+      </FadeInAnimation>
     </div>
   );
 }
